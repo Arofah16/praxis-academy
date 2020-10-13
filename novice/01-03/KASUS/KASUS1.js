fcfdb49d0ed4fe872283574d2d9b71a9cf6a7f52
Arofah16 <arofahr06@gmail.com>
2020-10-13 17:09:22 +0700

    last task

---
+++ novice/01-03/KASUS/KASUS1.js
@@ -0,0 +1,9 @@
+function mahasiswa(nama,alamat,tinggal,semester){
+    var data = ("Nama Saya "+nama+ " Alamat Saya "+alamat+" Saya Tinggal di "+tinggal+" Sekarang Saya Semester "+semester+"<br>");
+    document.write(data);
+
+        return mahasiswa;
+    }
+    mahasiswa(" Arofah" , " Purworejo" , " Yogyakarta", 7);
+    mahasiswa("kalila" , "wadas", "jakarta", 1);
+   
