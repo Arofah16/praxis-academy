4560b8379372924b564174811110a8ad97f1d88c
arofah16 <arofahr06@gmail.com>
2020-11-04 17:50:27 +0700

    hahaha

---
+++ NOVICE/03-05/project-new/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+
+      <v-list dense nav>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-image</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title> Image </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-account-alert-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+
+
+      </v-list>
+
+     
+
+
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+<script>
+export default {
+  nama: "app",
+  components: {},
+  data: () => ({}),
+};
+</script>
+
