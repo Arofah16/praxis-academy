7f8c9946da5bf080d96dcdce758caac3dd71470d
arofah16 <arofahr06@gmail.com>
2020-11-05 17:28:50 +0700

    tes

---
+++ NOVICE/03-05/KASUS/src/views/adress.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h1>HALAMAN UTAMA YANG DITUJU</h1>
+      <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+div{
+  border: 2px;
+  background-color: aqua;
+  padding: 30px;
+}
+
+</style>
---
+++ NOVICE/03-05/KASUS/src/views/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>Kontak yang bisa dihubungi</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
